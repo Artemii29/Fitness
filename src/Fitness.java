d20a4a2ba32910aab4aab8ac06dcbbb9be912ea4
Artemii29 <artemii.kharitonov@bk.ru>
2023-10-25 22:14:26 +0300

    new update

--- src/Fitness.java
+++ src/Fitness.java
@@ -7,15 +7,39 @@ public class Fitness {
     private int count3 = 0;
     private Membership[] poolMemberships = new Membership[20];
     private Membership[] groupMemberships = new Membership[20];
+    public boolean checkMembership(Membership membership) {
+        for (Membership m : gymMemberships) {
+            if (m == membership) {
+                return true;
+            }
+        }
+        for (Membership m : poolMemberships) {
+            if (m == membership) {
+                return true;
+            }
+        }
+        for (Membership m : groupMemberships) {
+            if (m == membership) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     public void addMembership(Membership membership) {
         if (membership.zone == Zone.POOL) {
-            if(membership instanceof Day){
+            if(membership.getType() == Type.DAY){
                 System.out.println("Day memberships are not allowed in the pool.");
             }
             else if (count1 < 20) {
-                poolMemberships[count1++] = membership;
-            } else {
+                for (int i = 0; i <count1;i++){
+                    if(poolMemberships[i] != membership){
+                        poolMemberships[count1++] = membership;
+                    }
+                }
+                }
+
+             else {
                 System.out.println("Мест нет");
             }
         } else if (membership.zone == Zone.GYM) {
--- src/Membership.java
+++ src/Membership.java
@@ -1,16 +1,44 @@
 import java.util.Date;
 
 public class Membership {
+    protected Type type;
+    protected Zone zone;
     protected int number;
     protected Owner owner;
     protected Date dateCreated;
     protected Date dateFinshed;
-    protected Zone zone;
-    protected Membership( int number, Owner owner, Date dateCreated, Date dateFinshed, Zone zone ){
+
+
+    public Zone getZone() {
+        return zone;
+    }
+    public int getNumber() {
+        return number;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public Date getDateFinshed() {
+        return dateFinshed;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    protected Membership(Type type, Zone zone, int number, Owner owner, Date dateCreated, Date dateFinshed ) {
+        this.type = type;
+        this.zone = zone;
         this.number = number;
         this.owner = owner;
         this.dateCreated = dateCreated;
         this.dateFinshed = dateFinshed;
-        this.zone = zone;
+
     }
 }
